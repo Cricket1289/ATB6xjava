da0eeea202eb4a4c3de734f6047e0f085423ae34
Cricket1289 <abhiram.kota799@gmail.com>
2024-04-29 08:50:02 +0530

    pushing

---
+++ src/lab14.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class lab14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter num1");
+        int ab2 = sc.nextInt();
+        System.out.println("enter num2");
+        int ab1 = sc.nextInt();
+        System.out.println("enter operation, +,%,/,*");
+        Character operator = sc.next().charAt(0);
+        switch (operator){
+            case '+':
+                System.out.println("result = "+ (ab2 + ab1));
+                break;
+            case '%':
+                System.out.println("result = "+ (ab1 % ab2));
+                break;
+            case '/':
+                System.out.println("result = "+ (ab1 / ab2));
+                break;
+            case '*':
+                System.out.println("result = "+ (ab1 * ab2));
+                break;
+
+        }
+    }
+}
