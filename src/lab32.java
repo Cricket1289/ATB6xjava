02b5e856fde2b9a381cfa156f4b8d14477387ecc
Cricket1289 <abhiram.kota799@gmail.com>
2024-05-06 10:12:06 +0530

    marks array

--- src/lab32.java
+++ src/lab32.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
-
 public class lab32 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for (int i=1;i<=100;i++) {
-            if (i % 3 == 0) {
-                System.out.println("fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
-            } else if (i % 3 == 0 && i % 5 == 0) {
+        for (int i=1;i<=100;i++){
+            if (i%3==0 && i%5==0) {
                 System.out.println("fizzbuzz");
-            } else {
-                System.out.println("not a value of 3 and 5");
+            }else if (i%3==0){
+                System.out.println("fizz");
+            } else if (i%5==0) {
+                System.out.println("buzz");
+            }else {
+                System.out.println(i);
             }
         }
     }
--- src/lab33.java
+++ src/lab33.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class lab33 {
     public static void main(String[] args) {
-        for (int i=1;i<=100;i++){
-            if (i%3==0 && i%5==0) {
-                System.out.println("fizzbuzz");
-            }else if (i%3==0){
-                System.out.println("fizz");
-            } else if (i%5==0) {
-                System.out.println("buzz");
-            }else {
-                System.out.println(i);
-            }
+        Scanner sc = new Scanner(System.in);
+        int [] marks = new int[5];
+        System.out.println("enter sub 1 marks");
+        marks[0] = sc.nextInt();
+        System.out.println("enter sub 2 marks");
+        marks[1] = sc.nextInt();
+        System.out.println("enter sub 3 marks");
+        marks[2] = sc.nextInt();
+        System.out.println("enter sub 4 marks");
+        marks[3] = sc.nextInt();
+        System.out.println("enter sub 5 marks");
+        marks[4] = sc.nextInt();
+        for (int i=0;i<marks.length;i++){
+            if (marks[i]>35){
+                System.out.println("passed");
+            }else
+            System.out.println("failed");
         }
     }
 }
