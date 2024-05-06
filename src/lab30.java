52d021cbf31a2c87a81f9129820dd6f3e359f6e0
Cricket1289 <abhiram.kota799@gmail.com>
2024-05-06 07:18:34 +0530

    --b

---
+++ src/lab30.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class lab30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = 10;
+        System.out.println(--b + b++ + ++b);
+    }
+}
