33156521d1b83c8878481d6874e9df727a0c99e2
Cricket1289 <abhiram.kota799@gmail.com>
2024-05-06 07:15:21 +0530

    a++

--- src/lab27.java
+++ src/lab27.java
@@ -1,2 +1,14 @@
 public class lab27 {
+    public static void main(String[] args) {
+        int[][] array_2d = {
+                {23,1,2},
+                {12,34,22},
+                {4,56,79}
+        };
+        for (int i=0;i<array_2d.length;i++){
+            for (int j=0;j< array_2d[i].length;j++){
+                System.out.println(array_2d[i][j]);
+            }
+        }
+    }
 }
